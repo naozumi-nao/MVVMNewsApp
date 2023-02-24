63f740dbeacf75a8591844ec94ed413e73d9a7a9
Bima Adityo Kurniawan <dito95.bluestars@gmail.com>
2023-02-24 21:00:49 +0700

    first commit, base layout, fragments & api

---
+++ app/src/main/java/com/example/mvvmnewsapp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mvvmnewsapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mvvmnewsapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
